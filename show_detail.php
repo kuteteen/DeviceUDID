698e5f6143fb0434f643dfdd775dc023a9ffb3d7
Will Mays <wmays132@gmail.com>
2016-07-26 17:48:59 -0400

    Obtain Devices UDID

---
+++ show_detail.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WMaysTech-UDID</title>
+        <meta name="viewport" content="width=device-width" />
+    </head>
+    <body>
+        <div>
+            <h1><center>Device Information</center></h1>
+                <p><center>UDID: <?php echo $_GET['UDID']; ?></center></p>
+                <p><center>Device product: <?php echo $_GET['DEVICE_PRODUCT']; ?></center></p>
+                <p><center>Device version: <?php echo $_GET['DEVICE_VERSION']; ?></center></p>
+        </div>
+    </body>
+</html>
